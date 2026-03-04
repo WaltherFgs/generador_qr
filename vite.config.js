d20e517dc7597bfd88dc4ac673a0c1562c7259e7
Walter <walter@Mac-mini-de-Walter.local>
2026-03-04 10:59:06 -0500

    wertyuio

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Permite conexiones desde fuera del contenedor/localhost
+    port: process.env.PORT ? parseInt(process.env.PORT) : 5173,
+  },
+  preview: {
+    host: '0.0.0.0', // Igual para el modo preview
+    port: process.env.PORT ? parseInt(process.env.PORT) : 4173,
+  }
 })
